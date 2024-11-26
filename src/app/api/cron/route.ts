7c27f2de38166ca7f575ee61a668f5904c6d163d
lukaszpiera <l.piera@proton.me>
2024-11-26 18:45:43 +0100

    feat: update cron job endpoint for database seeding and enhance authorization handling

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,14 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import { seedDatabase } from "~/server/db/seed";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+export async function GET(request: Request, response: Response) {
+  if (
+    request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json({
+      error: "Unauthorized, only vercel can access",
+      status: 401,
+    });
+  }
+
   try {
     await seedDatabase();
-    res.status(200).json({ message: "Database seeded successfully." });
+    return NextResponse.json({ status: 200, message: "Database updated" });
   } catch (error) {
-    res.status(500).json({ error: "Seeding failed." });
+    return NextResponse.json({ status: 500, error: "Internal server error" });
   }
 }
