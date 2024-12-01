638b0e4db384c310d5e7d81d50fbb9125de84fe5
lukaszpiera <l.piera@proton.me>
2024-12-01 21:13:31 +0100

    refactor: remove middleware logic for skipping /api/cron endpoint

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,6 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  if (pathname === "/api/cron") {
-    console.log("[Middleware] Skipping /api/cron.");
-    return new Response(null);
-  }
-
   await updateSession(request);
 }
 
