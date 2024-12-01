993400c0382169da6ddd941247c5432d63be34c9
lukaszpiera <l.piera@proton.me>
2024-12-01 20:55:50 +0100

    feat: add logging for authorization checks in cron API endpoint

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,15 +1,19 @@
 import type { NextRequest } from "next/server";
 
 export function GET(request: NextRequest) {
+  console.log("Received request to /api/cron");
+
   const authHeader = request.headers.get("authorization");
   if (
     process.env.NODE_ENV === "production" &&
     authHeader !== `Bearer ${process.env.CRON_SECRET}`
   ) {
+    console.log("Unauthorized access attempt to /api/cron");
     return new Response("Unauthorized", {
       status: 401,
     });
   }
 
+  console.log("Authorized cron job executed successfully");
   return Response.json({ success: "I am cron" });
 }
